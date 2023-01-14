f7e0ce9b960b5643667870cbeab59408d1978039
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-14 15:28:34 +0900

    Add files via upload

---
+++ Section.16/Conditionals/app.js
@@ -0,0 +1,39 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+// } else {
+//     console.log("YOUR NUMBER IS GRETER (OR EQUL) THAN 0.5!!! ")
+// }
+// console.log(random);
+
+
+// const dayOfweek = prompt('ENTER A DAY').toLowerCase();
+
+// if (dayOfweek === 'monday') {
+//     console.log("UGHHH I HATE MONDAYS!")
+// } else if (dayOfweek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS!")
+// } else if (dayOfweek === 'firday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!!")
+// } else {
+//     console.log("MEH")
+// }
+
+
+
+// 0-5 - FREE
+// 5-10 CHILD $10
+// 10-65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 8;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for free!")
+} else if (age < 10) {
+    console.log("You are a child. You pay $10")
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20")
+} else {
+    console.log("You are a senior. You pay $10")
+}
