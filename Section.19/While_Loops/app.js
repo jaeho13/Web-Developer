774de5398f8bb73e9e8063d8723b550975e401a7
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-19 17:18:24 +0900

    Add files via upload

---
+++ Section.19/While_Loops/app.js
@@ -0,0 +1,33 @@
+// let count = 0;
+// while (count < 10) {
+//     count++;
+//     console.log(count)
+// }
+
+// const SECRET = "BabyHippo"
+
+// let guess = prompt("enter the secret code...");
+// while (guess !== SECRET) {
+//     guess = prompt("enter the secret code...");
+// }
+// console.log("CONGRATS YOU GOT THE SECRET!!!")
+
+
+// let input = prompt("Hey, say something!")
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === "stop copying me") break;
+// }
+// console.log("OK YOU WIN!")
+
+
+for (let i = 0; i < 1000; i++) {
+    console.log(i);
+    if (i === 100) break;
+}
+
+
+
+
+// while 과 for 문의 차이
+// while문은 반복의 횟수를 정하지 않아도 되기에 비밀번호 맞추기 등에 유리하다
