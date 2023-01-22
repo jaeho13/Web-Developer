54f3200cd1c0b86ade40c03b2f304439e4aba023
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-22 13:28:06 +0900

    Add files via upload

---
+++ Section.20/app.js
@@ -0,0 +1,30 @@
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+singSong()
+
+
+
+
+
+
+
+
+// Coding Exercise 35 : Heart Function Exercise
+
+// function printHeart() {
+//     console.log('<3');
+// }
+// console.log('<3');
