0f0614ccbf3be8bceaab33b75157e938e7c11e14
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-23 15:58:09 +0900

    Add files via upload

---
+++ Section.19/ToDoList/todos.js
@@ -0,0 +1,13 @@
+let input = prompt('what would you like to do?')
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('*****************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*****************')
+    }
+    input = prompt('what would you like to do?')
+}
+console.log("OK QUIT THE APP!")
