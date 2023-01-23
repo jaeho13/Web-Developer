44fc7bba17456119c5d202ab7f16d2930e0e3da9
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-23 23:45:51 +0900

    Add files via upload

--- Section.20/app.js
+++ Section.20/app.js
@@ -1,19 +1,39 @@
-function singSong() {
-    console.log("DO");
-    console.log("RE");
-    console.log("MI");
+// function singSong() {
+//     console.log("DO");
+//     console.log("RE");
+//     console.log("MI");
+// }
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`);
 }
 
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
-singSong()
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
 
 
 
@@ -28,3 +48,25 @@ singSong()
 //     console.log('<3');
 // }
 // console.log('<3');
+
+
+// Coding Exercise 36 : Rant Exercise
+
+// function rant(message) {
+//     console.log(message.toUpperCase(message))
+//     console.log(message.toUpperCase(message))
+//     console.log(message.toUpperCase(message))
+// }
+
+// Coding Exercise 37 : Multiple Args Exercise
+
+// function isSnakeEyes(num1, num2) {
+//     if (num1 === 1 && num2 === 1) {
+//         console.log("Snake Eyes!");
+//     } else {
+//         console.log("Not Snake Eyes!")
+//     }
+// }
+
+// let dice = Math.floor(Math.random() * 6) + 1;
+// isSnakeEyes(dice, dice);
