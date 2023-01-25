97ddbfe7387ecfc9bba815e944ffb1006e49594c
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-25 12:18:33 +0900

    Add files via upload

---
+++ Section.21/app.js
@@ -0,0 +1,22 @@
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch()
+
+for (let i = 0; i < 5; i++) {
+    let msg = "SeongGong";
+    console.log(msg)
+}
+console.log(msg)
+console.log(i)
