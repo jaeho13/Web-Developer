cd55511ebd20208554df458a607b524f70b49b65
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-01-25 12:57:25 +0900

    Add files via upload

---
+++ Section.21/FunctionExpressions/app.js
@@ -0,0 +1,15 @@
+// function add(x, y) {
+//     return x + y;
+// }
+
+const add = function (x, y) {
+    return x + y;
+}
+
+
+
+// Coding Exercise 44 : Function Expression Exercise
+
+// const square = function (x) {
+//     return x * x;
+// }
