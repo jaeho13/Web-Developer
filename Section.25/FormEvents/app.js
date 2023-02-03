f29d7614f8e1697bb47fca9d7b51596edbca064b
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-03 15:15:20 +0900

    Add files via upload

---
+++ Section.25/FormEvents/app.js
@@ -0,0 +1,22 @@
+const tweetForm = document.querySelector('#tweetForm');
+const tweetsContainer = document.querySelector('#tweets');
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // const usernameInput = document.querySelectorAll('input')[0];
+    // const tweetInput = document.querySelectorAll('input')[1];
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(usernameInput.value, tweetInput.value)
+    usernameInput.value = '';  //입력하고 빈 칸으로 바꿔주는 역할
+    tweetInput.value = '';  //입력하고 빈 칸으로 바꿔주는 역할
+});
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)
+    newTweet.append(bTag);
+    newTweet.append(`- ${tweet}`);
+    tweetsContainer.append(newTweet);
+}
