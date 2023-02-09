47354950aff8387590468bcd7e71455b954be7a4
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-10 02:06:59 +0900

    Add files via upload

--- Section.28/Axios/app.js
+++ Section.28/Axios/app.js
@@ -5,14 +5,38 @@
 //         console.log("ERROR! ", e);
 //     });
 
-const getStarWarsPerson = async () => {
-    try {
-        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-        console.log(res.data);
-    } catch (e) {
-        console.log("ERROR", e);
-    }
-};
+// const getStarWarsPerson = async () => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//         console.log(res.data);
+//     } catch (e) {
+//         console.log("ERROR", e);
+//     }
+// };
 
-getStarWarsPerson(5);
-getStarWarsPerson(10);
+// getStarWarsPerson(5);
+// getStarWarsPerson(10);
+
+const jokes = document.querySelectorAll('#jokes');
+const button = document.querySelector('button');
+
+
+const addNewJoke = async () => {
+    const jokeText = getDadJoke();
+    console.log(jokeText)
+    const newLI = document.createElement('LI');
+    newLI.append(res.data.joke);
+    jokes.append(newLI)
+}
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
+    }
+
+}
+
+button.addEventListener('click', addNewJoke)
