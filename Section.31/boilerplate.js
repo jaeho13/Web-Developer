384a43f63293c0cc8e48b39a25f9a094a32c7845
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-15 01:09:23 +0900

    Add files via upload

--- Section.31/boilerplate.js
+++ Section.31/boilerplate.js
@@ -6,7 +6,12 @@ const folderName = process.argv[2] || 'Project'
 //     if (err) throw err;
 // });
 
-fs.mkdirSync(folderName);
-fs.writeFileSync(`${folderName}/index.html`)
-fs.writeFileSync(`${folderName}/app.js`)
-fs.writeFileSync(`${folderName}/styles.css`)
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`)
+    fs.writeFileSync(`${folderName}/app.js`)
+    fs.writeFileSync(`${folderName}/styles.css`)
+} catch (e) {
+    console.log("SOMETHING WENT WRONG!!!")
+    console.log(e);
+}
