ad047f9c2dad3662dd1019b08e69c7aee07f221d
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-16 23:47:37 +0900

    Add files via upload

--- Section.34/index.js
+++ Section.34/index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit })
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', { num })
+})
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
 })
