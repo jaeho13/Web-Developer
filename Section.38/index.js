e21f7bb32c65f10f3ddad7b4cfc75a0bcbbc0048
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-25 02:30:05 +0900

    Add files via upload

--- Section.38/index.js
+++ Section.38/index.js
@@ -23,13 +23,15 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
 
+const categories = ['fruit', 'vegetable', 'dairy']
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.post('/products', async (req, res) => {
@@ -37,7 +39,6 @@ app.post('/products', async (req, res) => {
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`)
 })
-
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
@@ -48,7 +49,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product })
+    res.render('products/edit', { product, categories })
 })
 
 app.put('/products/:id', async (req, res) => {
@@ -56,18 +57,6 @@ app.put('/products/:id', async (req, res) => {
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true });
     res.redirect(`/products/${product._id}`);
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
