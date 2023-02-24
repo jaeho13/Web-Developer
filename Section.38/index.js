d31754001acb9ec5703d195e1367a581712c1054
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-25 00:52:38 +0900

    Add files via upload

--- Section.38/index.js
+++ Section.38/index.js
@@ -18,16 +18,27 @@ mongoose.connect("mongodb://127.0.0.1:27017/farmStand", { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
 })
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id)
-    console.log(product);
     res.render('products/show', { product })
 })
 
