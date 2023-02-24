731055e858b8bae0e1cfef60a4786c70d01eb7f1
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-24 18:34:56 +0900

    Add files via upload

---
+++ Section.38/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',true);
+
+
+const Product = require('./models/product')
+
+mongoose.connect("mongodb://127.0.0.1:27017/farmStand", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!")
+  })
+  .catch(err => {
+    console.log("ON NO MONGO CONNECTION ERROR!!!!")
+    console.log(err)
+  })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000!")
+})
