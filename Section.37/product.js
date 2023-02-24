1a690895ee109368f2e56160c376bb08fd94385e
jaeho13 <111284065+jaeho13@users.noreply.github.com>
2023-02-24 12:06:53 +0900

    Add files via upload

--- Section.37/product.js
+++ Section.37/product.js
@@ -13,23 +13,50 @@ mongoose.connect("mongodb://127.0.0.1:27017/shopApp", { useNewUrlParser: true, u
   const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 20
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: [0, 'Price must be positive ya dodo!']
+    },
+    onSale: {
+      type: Boolean,
+      default: false
+    },
+    categories: [String],
+    qty: {
+      online: {
+        type: Number,
+        default: 0
+      },
+      inStore: {
+        type: Number,
+        default: 0
+      }
     }
+
   });
 
-  const Product = mongoose.model('Product', productSchema);
+  // const Product = mongoose.model('Product', productSchema);
 
-  const bike = new Product({ name: 'Mountain Bike', price: 999, color: 'red' })
-  bike.save()
+  // const bike = new Product({ name: 'Tire Pump', price: 19.50, categories: ['Cycling'] })
+  // bike.save()
+  //   .then(data => {
+  //       console.log("IT WORKED!")
+  //       console.log(data);
+  //   })
+  //   .catch(err => {
+  //       console.log("OH NO ERROR!")
+  //       console.log(err)
+  //   })
+Product.findOneAndUpdate({ name: 'Tire Pump'}, { price: 100}, { new: true })
     .then(data => {
-        console.log("IT WORKED!")
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!")
-        console.log(err)
-    })
+      console.log("IT WORKED!")
+      console.log(data);
+  })
+  .catch(err => {
+      console.log("OH NO ERROR!")
+      console.log(err)
+  })
